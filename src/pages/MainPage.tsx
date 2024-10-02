e111e30c69d8038d83be3d578ca1754f746a7d81
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2024-10-02 19:58:37 +0500

    fixes

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -24,25 +24,29 @@ const MainPage = () => {
                     className='flex justify-end'
                     onClick={togglePopup}
                 >
-                    <Trash 
-                        className='border-2 rounded cursor-pointer'
-                    />
+                    <div className='p-1'>
+                        <Trash 
+                            className='cursor-pointer'
+                        />
+                    </div>
                 </div>
                 {displayTasks}
-                <div className='p-2 flex items-center gap-3 w-fit border-2 rounded font-medium box-border cursor-pointer active:bg-lb hover:border-black'>
+                <div 
+                    className='cursor-pointer outline-none p-2 rounded-lg w-full hover:outline-custom duration-300 hover:text-custom
+                        p-2 flex items-center gap-3 w-fit rounded font-medium active:bg-lb select-none active:bg-lb hover:bg-lb'
+                    onClick={() => formStore.isFormOpen = true}    
+                >
                     <span>+</span>
-                    <span
-                        onClick={() => formStore.isFormOpen = true}
-                    >
+                    <span className=''>
                         Добавить задачу
                     </span>
                 </div>
             </div>
             {taskStore.isDeletePopupOpen && (
-                <div className="absolute h-full w-full flex items-center justify-center">
-                    <div className='flex flex-col items-start border-2 border-black rounded p-2 bg-lb gap-2'>
+                <div className="absolute h-full w-full flex items-center justify-center select-none">
+                    <div className='flex flex-col items-start border-2 border-black rounded-md p-4 bg-lb gap-3 text-left border-inherit'>
                         <span 
-                            className='hover:text-red-500 cursor-pointer hover:duration-300 duration-300'
+                            className='cursor-pointer outline-none p-2 rounded-lg w-full hover:outline-custom active:outline-custom duration-300 hover:text-custom'
                             onClick={() => {
                                 taskStore.deleteAllTasks()
                                 togglePopup()
@@ -51,7 +55,7 @@ const MainPage = () => {
                             Удалить все
                         </span>
                         <span 
-                            className='hover:text-orange-400 cursor-pointer hover:duration-300 duration-300'
+                            className='cursor-pointer outline-none p-2 rounded-lg w-full hover:outline-custom duration-300 hover:text-custom'
                             onClick={() => {
                                 taskStore.deleteSelectedTasks()
                                 togglePopup()
@@ -59,7 +63,12 @@ const MainPage = () => {
                         >
                             Удалить выбранное
                         </span>
-                        <span className='hover:text-grey cursor-pointer hover:duration-300 duration-300' onClick={togglePopup}>Закрыть</span>
+                        <span 
+                            className='cursor-pointer outline-none p-2 rounded-lg w-full hover:outline-custom duration-300 hover:text-custom'
+                            onClick={togglePopup}
+                        >
+                            Закрыть
+                        </span>
                     </div>
                 </div>
             )}
@@ -69,20 +78,22 @@ const MainPage = () => {
             <div className='min-w-half min-h-screen bg-grey'>
                 <div className='p-9'>
                     {taskStore.selectedTask ? (
-                        <div className='w-fit flex flex-col items-start	text-lg font-normal gap-5'>
-                            <div className='flex h-10 items-center gap-2'>
+                        <div className='flex flex-col items-start text-lg font-normal gap-5'>
+                            <div className='flex h-10 items-center gap-2 w-full'>
                                 <input 
                                     value={taskStore.selectedTask.name} 
                                     disabled={!taskStore.isEditName} 
                                     onChange={(e) => taskStore.updateTaskField('name', e.target.value)}
+                                    className='rounded box-border outline-none p-2 max-w-85 h-85'
                                 />
                                 <Edit className='w-5 h-5 cursor-pointer' onClick={() => taskStore.toggleEditField('name')}/>
                             </div>
-                            <div className='flex h-10 items-center gap-2'>
+                            <div className='flex h-10 items-center gap-2 w-full'>
                                 <input 
                                     value={taskStore.selectedTask.description} 
                                     disabled={!taskStore.isEditDescription}
                                     onChange={(e) => taskStore.updateTaskField('description', e.target.value)}
+                                    className='rounded box-border outline-none p-2 max-w-85 h-85'
                                 />
                                 <Edit className='w-5 h-5 cursor-pointer' onClick={() => taskStore.toggleEditField('description')}/>
                             </div>
